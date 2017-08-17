1a1803d75755fbf04f5e70aaf6e641427a478914
BJCreslin <bjcreslin@gmail.com>
2017-08-17 19:38:53 +0700

    Initial commit

---
+++ src/Solution.java
@@ -0,0 +1,27 @@
+import java.io.File;
+
+/**
+ * Created by kvu on 17.08.2017.
+ */
+public class Solution {
+    public static void main(String[] args) {
+        String[] massiv_strok = new String[]{
+                "a//b//..//file.txt",
+                "a////b////c////file.txt",
+                "a////..////b////c////file.txt",
+                "a////.////b////..////c////.////file.txt",
+                ".////a////b////..////b////c////.////file.txt"};
+
+        for (String stroka_podstanovki : massiv_strok
+                ) {
+
+            File file_temp = new File(stroka_podstanovki);
+            System.out.println(stroka_podstanovki);
+           try {
+                System.out.println(file_temp.getCanonicalPath());
+            } catch (Exception e) {
+            }
+            System.out.println("");
+        }
+    }
+}
